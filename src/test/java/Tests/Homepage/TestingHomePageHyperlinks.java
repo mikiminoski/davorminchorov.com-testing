4659d2c79e94611ce85b6e2ab4369679e1c07776
Miki Minoski <mikiminoski@gmail.com>
2021-03-16 14:08:00 +0100

    Add manual test design file, update the coments with test IDs of the tests

--- src/test/java/Tests/Homepage/TestingHomePageHyperlinks.java
+++ src/test/java/Tests/Homepage/TestingHomePageHyperlinks.java
@@ -9,6 +9,9 @@ public class TestingHomePageHyperlinks extends BaseSet {
 
     HomePage homepage = new HomePage();
 
+    /**
+     * Test ID: n13, n14
+     */
     @Test
     public void validate_quantox_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.linkText("Quantox Technology")));
@@ -16,6 +19,9 @@ public class TestingHomePageHyperlinks extends BaseSet {
         homepage.clickAndCheckLink("quantox.com");
     }
 
+    /**
+     * Test ID: n15, n16
+     */
     @Test
     public void validate_adeva_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.linkText("Adeva, an exclusive remote developers' network")));
@@ -23,6 +29,9 @@ public class TestingHomePageHyperlinks extends BaseSet {
         homepage.clickAndCheckLink("adevait.com");
     }
 
+    /**
+     * Test ID: n17, n18
+     */
     @Test
     public void validate_tabellarius_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.linkText("Tabellarius")));
@@ -30,6 +39,9 @@ public class TestingHomePageHyperlinks extends BaseSet {
         homepage.clickAndCheckLink("tabellarius.mk");
     }
 
+    /**
+     * Test ID: n19, n20
+     */
     @Test
     public void validate_intertec_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.linkText("Intertec")));
@@ -37,6 +49,9 @@ public class TestingHomePageHyperlinks extends BaseSet {
         homepage.clickAndCheckLink("www.intertec.io");
     }
 
+    /**
+     * Test ID: n21, n22
+     */
     @Test
     public void validate_adeva_bootcamp_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.linkText("Adeva's Female Bootcamp 2018")));
@@ -44,6 +59,9 @@ public class TestingHomePageHyperlinks extends BaseSet {
         homepage.clickAndCheckLink("femalebootcamp.adevait.com");
     }
 
+    /**
+     * Test ID: n23, n24
+     */
     @Test
     public void validate_check_here_hyperlink_after_adeva_bootcamp() {
         homepage.findHyperlink(driver.findElement(By.linkText("here")));
@@ -51,18 +69,27 @@ public class TestingHomePageHyperlinks extends BaseSet {
         homepage.clickAndCheckLink("blog.usejournal.com/the-tech-bootcamp-experience-ff53367b79a7");
     }
 
+    /**
+     * Test ID: n25
+     */
     @Test
     public void validate_linkedIn_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.xpath("/html/body/div/footer/a[1]")));
         homepage.clickAndCheckLink("www.linkedin.com/in/davorminchorov");
     }
 
+    /**
+     * Test ID: n26
+     */
     @Test
     public void validate_gitHub_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.xpath("/html/body/div/footer/a[2]")));
         homepage.clickAndCheckLink("github.com/davorminchorov");
     }
 
+    /**
+     * Test ID: n27
+     */
     @Test
     public void validate_twitter_hyperlink() {
         homepage.findHyperlink(driver.findElement(By.xpath("/html/body/div/footer/a[3]")));
--- src/test/java/Tests/Homepage/TestingHomePageTextsAndBlogNavigation.java
+++ src/test/java/Tests/Homepage/TestingHomePageTextsAndBlogNavigation.java
@@ -8,22 +8,34 @@ public class TestingHomePageTextsAndBlogNavigation extends BaseSet {
 
     HomePage homepage = new HomePage();
 
+    /**
+     * Test ID: n3
+     */
     @Test
     public void click_on_blog_and_validate_url() {
         homepage.findBlog();
         homepage.clickBlog().validateBlogUrl();
     }
 
+    /**
+     * Test ID: n2
+     */
     @Test
     public void validate_home_screen() {
         homepage.validateScreen();
     }
 
+    /**
+     * Test ID: n4
+     */
     @Test
     public void check_about_me_text() {
         homepage.checkAboutMe();
     }
 
+    /**
+     * Test ID: n5
+     */
     @Test
     public void check_technology_stack_text() {
         homepage.checkTechnologyStackText();
